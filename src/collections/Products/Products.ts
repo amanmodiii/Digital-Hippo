c97905c9d2d91646e13c519961785ae211474d2c
amanmodiii <amanmodi290903@gmail.com>
2024-06-30 21:35:08 +0530

    final changes for security

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,4 +1,5 @@
 import {
+  AfterChangeHook,
   BeforeChangeHook,
 } from 'payload/dist/collections/config/types'
 import { PRODUCT_CATEGORIES } from '../../config'
@@ -15,14 +16,80 @@ const addUser: BeforeChangeHook<Product> = async ({
   return { ...data, user: user.id }
 }
 
+const syncUser: AfterChangeHook<Product> = async ({
+  req,
+  doc,
+}) => {
+  const fullUser = await req.payload.findByID({
+    collection: 'users',
+    id: req.user.id,
+  })
 
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
+
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === 'object' ? product.id : product
+      ) || []),
+    ]
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
+
+    const dataToUpdate = [...createdProductIDs, doc.id]
+
+    await req.payload.update({
+      collection: 'users',
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    })
+  }
+}
+
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
+
+    if (!user) return false
+    if (user.role === 'admin') return true
+
+    const userProductIDs = (user.products || []).reduce<
+      Array<string>
+    >((acc, product) => {
+      if (!product) return acc
+      if (typeof product === 'string') {
+        acc.push(product)
+      } else {
+        acc.push(product.id)
+      }
+
+      return acc
+    }, [])
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    }
+  }
 
 export const Products: CollectionConfig = {
   slug: 'products',
   admin: {
     useAsTitle: 'name',
   },
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
+    afterChange: [syncUser],
     beforeChange: [
       addUser,
       async (args) => {
@@ -33,7 +100,7 @@ export const Products: CollectionConfig = {
             await stripe.products.create({
               name: data.name,
               default_price_data: {
-                currency: 'INR',
+                currency: 'USD',
                 unit_amount: Math.round(data.price * 100),
               },
             })
@@ -89,9 +156,9 @@ export const Products: CollectionConfig = {
     },
     {
       name: 'price',
-      label: 'Price in INR',
+      label: 'Price in USD',
       min: 0,
-      max: 100000,
+      max: 1000,
       type: 'number',
       required: true,
     },
