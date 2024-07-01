34c6b5e64891151391767c73eaef79e965eec5f0
amanmodiii <amanmodi290903@gmail.com>
2024-07-01 19:19:53 +0530

    server.ts updated

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,74 @@
-import express from "express"
-import { getPayloadClient } from "./get-payload"
-import { nextApp, nextHandler } from "./next-utils"
-import payload from "payload"
-import * as trpcExpress from "@trpc/server/adapters/express"
-import { appRouter } from "./trpc"
-import { inferAsyncReturnType } from "@trpc/server"
-import bodyParser from "body-parser"
-import { IncomingMessage } from "http"
-import { stripeWebhookHandler } from "./webhooks"
-import nextBuild from "next/dist/build"
-import path from "path"
-import { PayloadRequest } from "payload/types"
-import { parse } from "url"
+import express from 'express'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './trpc'
+import { inferAsyncReturnType } from '@trpc/server'
+import bodyParser from 'body-parser'
+import { IncomingMessage } from 'http'
+import { stripeWebhookHandler } from './webhooks'
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
-const createContext = ({req,res}: trpcExpress.CreateExpressContextOptions) => ({req,res})
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+})
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
 
 export type WebhookRequest = IncomingMessage & {
   rawBody: Buffer
 }
 
 const start = async () => {
-    const webhookMiddleware = bodyParser.json({
-        verify: (req: WebhookRequest, _, buffer) => {
-            req.rawBody = buffer
-        },
-    })
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
 
-    const payload = await getPayloadClient({
-        initOptions: {
-            express: app,
-            onInit: async(cms)=>{
-                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
-            },
-        },
-    })
-
-    app.post(
+  app.post(
     '/api/webhooks/stripe',
     webhookMiddleware,
     stripeWebhookHandler
-    )
+  )
 
-    const cartRouter = express.Router()
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      },
+    },
+  })
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        'Next.js is building for production'
+      )
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
+    })
+
+    return
+  }
+
+  const cartRouter = express.Router()
 
   cartRouter.use(payload.authenticate)
 
@@ -63,36 +85,25 @@ const start = async () => {
   })
 
   app.use('/cart', cartRouter)
-
-    if (process.env.NEXT_BUILD) {
-    app.listen(PORT, async () => {
-      payload.logger.info(
-        'Next.js is building for production'
-      )
-
-      // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
-
-      process.exit()
-    })
-
-    return
-    }
-
-    app.use('/api/trpc',trpcExpress.createExpressMiddleware({
+  app.use(
+    '/api/trpc',
+    trpcExpress.createExpressMiddleware({
       router: appRouter,
       createContext,
     })
-    )
-    app.use((req,res) => nextHandler(req, res))
+  )
 
-    nextApp.prepare().then(()=>{
-        payload.logger.info("Next.js started")
+  app.use((req, res) => nextHandler(req, res))
 
-        app.listen(PORT, async () => {
-            payload.logger.info(`App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
-        })
+  nextApp.prepare().then(() => {
+    payload.logger.info('Next.js started')
+
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      )
     })
+  })
 }
 
 start()
